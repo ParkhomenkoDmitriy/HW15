768638ee683f534fc1b2d8b279af64d86a59ed4e
ParkhomenkoDmitriy <mazzzarahha@gmail.com>
2024-04-18 00:10:29 +0300

    translation into English

--- HW15.py
+++ HW15.py
@@ -1,6 +1,6 @@
 def entered_number(input_str):
     if input_str == '0':
-        return "Ви ввели нуль"
+        return "You entered zero"
 
     if '-' in input_str:
         if '.' in input_str or ',' in input_str:
@@ -9,13 +9,13 @@ def entered_number(input_str):
                 if len(number[0]) == 1 and number[0] == '-':
                     number[0] += '0'
                 if number[0].replace('-', '').isdigit() and number[1].isdigit():
-                    return f"Ви ввели від'ємне дробове число: {input_str}"
+                    return f"You entered a negative fractional number: {input_str}"
                 else:
-                    return f"Ви ввели неправильне число: {input_str}"
+                    return f"You have entered an incorrect number: {input_str}"
             else:
-                return f"Ви ввели неправильне число: {input_str}"
+                return f"You have entered an incorrect number: {input_str}"
         else:
-            return f"Ви ввели від'ємне ціле число: {input_str}"
+            return f"You entered a negative integer: {input_str}"
 
     if '.' in input_str or ',' in input_str:
         if input_str.count('.') == 1 or input_str.count(',') == 1:
@@ -23,21 +23,21 @@ def entered_number(input_str):
             if len(number[0]) == 0:
                 number[0] = '0'
             if number[0].isdigit() and number[1].isdigit():
-                return f"Ви ввели позитивне дробове число: {input_str}"
+                return f"You entered a positive fractional number: {input_str}"
             else:
-                return f"Ви ввели неправильне число: {input_str}"
+                return f"You have entered an incorrect number: {input_str}"
         else:
-            return f"Ви ввели неправильне число: {input_str}"
+            return f"You have entered an incorrect number: {input_str}"
     elif input_str.isdigit():
-        return f"Ви ввели позитивне ціле число: {input_str}"
+        return f"You entered a positive integer: {input_str}"
     else:
-        return f"Ви ввели неправильне число: {input_str}"
+        return f"You have entered an incorrect number: {input_str}"
 
 
 while True:
-    user_input = input("Введіть число або 'вихід', 'exit', 'quit', 'e' або 'q' для виходу: ").lower()
+    user_input = input("Enter a number or 'вихід', 'exit', 'quit', 'e' or 'q' to exit: ").lower()
     if user_input in ['вихід', 'exit', 'quit', 'e', 'q']:
-        print("Програма завершена.")
+        print("The program is complete.")
         break
     else:
         result = entered_number(user_input)
