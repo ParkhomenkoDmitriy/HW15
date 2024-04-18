838cf98e61baeed0b4f7d34780504efdae17003a
ParkhomenkoDmitriy <mazzzarahha@gmail.com>
2024-04-19 00:15:12 +0300

    import re re.match(r'^-?\d*(?:[\.,]?\d+)?$', input_str) #для проверки если введенная строка начинается с дефиса и содержит только цифры и один разделитель

--- HW15.py
+++ HW15.py
@@ -1,38 +1,24 @@
+import re
+
 def entered_number(input_str):
-    if input_str == '0':
-        return "You entered zero"
-
-    if '-' in input_str:
-        if '.' in input_str or ',' in input_str:
-            if input_str.count('.') == 1 or input_str.count(',') == 1:
-                number = input_str.split('.') if '.' in input_str else input_str.split(',')
-                if len(number[0]) == 1 and number[0] == '-':
-                    number[0] += '0'
-                if number[0].replace('-', '').isdigit() and number[1].isdigit():
-                    return f"You entered a negative fractional number: {input_str}"
-                else:
-                    return f"You have entered an incorrect number: {input_str}"
+    if re.match(r'^-?\d*(?:[\.,]?\d+)?$', input_str):
+        if input_str.replace(',', '').replace('.', '') == '0':
+            return "You entered zero 0"
+        if input_str.startswith('-') and '.' not in input_str and ',' not in input_str:
+            if input_str.lstrip('-0') == '':
+                return "You entered zero 0"
             else:
-                return f"You have entered an incorrect number: {input_str}"
-        else:
-            return f"You entered a negative integer: {input_str}"
-
-    if '.' in input_str or ',' in input_str:
-        if input_str.count('.') == 1 or input_str.count(',') == 1:
-            number = input_str.split('.') if '.' in input_str else input_str.split(',')
-            if len(number[0]) == 0:
-                number[0] = '0'
-            if number[0].isdigit() and number[1].isdigit():
-                return f"You entered a positive fractional number: {input_str}"
+                return f"You entered a negative integer: {input_str}"
+        if input_str.startswith('-'):
+            return f"You entered a negative fractional number: {input_str}"
+        if '.' not in input_str and ',' not in input_str:
+            if input_str.lstrip('0') == '':
+                return "You entered zero 0"
             else:
-                return f"You have entered an incorrect number: {input_str}"
-        else:
-            return f"You have entered an incorrect number: {input_str}"
-    elif input_str.isdigit():
-        return f"You entered a positive integer: {input_str}"
-    else:
-        return f"You have entered an incorrect number: {input_str}"
+                return f"You entered a positive integer: {input_str}"
+        return f"You entered a positive fractional number: {input_str}"
 
+    return f"You have entered an incorrect number: {input_str}"
 
 while True:
     user_input = input("Enter a number or 'вихід', 'exit', 'quit', 'e' or 'q' to exit: ").lower()
